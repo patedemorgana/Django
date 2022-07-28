4df4ec294c177824b541fa6991d9c0789b09e750
patedemorgana <gabriel.volpate@gmail.com>
2022-07-28 01:12:18 -0300

    dgfdfg

--- FaesaNews/FaesaNews/urls.py
+++ FaesaNews/FaesaNews/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
+from django.conf import Settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('main.urls'))
 ]
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,
