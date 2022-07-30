15d39fe0c62823041a418ebde7c10016b780e79d
patedemorgana <gabriel.volpate@gmail.com>
2022-07-29 23:52:44 -0300

    bnm

--- FaesaNews/main/views.py
+++ FaesaNews/main/views.py
@@ -1,6 +1,7 @@
 import email
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
 
 def index(request):
     return render(request, 'index.html')
