4759cfdbb0e35da9542122bd8fe08e21f0031dc6
patedemorgana <gabriel.volpate@gmail.com>
2022-07-29 23:55:12 -0300

    dfgdfg

--- FaesaNews/main/views.py
+++ FaesaNews/main/views.py
@@ -1,7 +1,8 @@
 import email
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 def index(request):
     return render(request, 'index.html')
@@ -12,5 +13,11 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']   
+
+        if password == password2:
+            pass
+        else:
+            messages.info(request, 'Senhas não coincidem, tente de novo')
+            return redirect(signup)
     else:
         return render(request, 'signup.html')
